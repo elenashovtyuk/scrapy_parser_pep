42cfb8afbb93c6d9fa1cc5ab69ce67e525715c2d
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-06-13 00:25:11 +0400

    delete comments

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -5,30 +5,18 @@ from pep_parse.items import PepParseItem
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/', ]
 
-    # имеем дело с обработкой страниц с разной структурой
-    # метод parse() должен собирать ссылки на документы PEP
     def parse(self, response):
-        # находим все ссылки на документы PEP
+        """Собирает ссылки на все документы PEP."""
         all_docs = response.css('#numerical-index td a::attr(href)').getall()
-        # перебираем в цикле ссылки на документы PEP
-        # и для каждой ссылки на документ нам нужно осуществить:
-        # переход по этой ссылке, т.е загрузку страницы для каждого документа
-        # и вызов метода parse_pep для каждой из них
-        # чтобы спарсить нужную информацию о каждом документе
-        # Возвращаем response.follow() с вызовом метода parse_pep()
         for doc_link in all_docs:
             yield response.follow(doc_link, callback=self.parse_pep)
 
-    # парсит страницы документов PEP по собранным ссылкам
     def parse_pep(self, response):
-        # парсинг страницы документа PEP
-        # сохранение спарсенных данных в словарь и
-        # возврат полученных данных в виде Items
-
-        # номер и название документа находятся в одной строке и разделены
-        # символом "-"
+        """
+        Парсит страницы документов PEP по полученным ссылкам и формирует Items.
+        """
         number, name = response.css('h1.page-title::text').get().split(' – ')
         data = {
             'number': number.replace('PEP ', ''),
@@ -36,6 +24,4 @@ class PepSpider(scrapy.Spider):
             'status': response.css(
                 'dt:contains("Status") + dd abbr::text').get(),
         }
-        # словарь с полученными данными передаем в конструктор класса
-        # PepParseItem
         yield PepParseItem(data)
